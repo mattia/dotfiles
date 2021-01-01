bcd1b7d7e2289e4922bf9d79197ee58515731c92
Mattia Valzelli <mattia@mvalzelli.com>
2021-01-01 16:24:45 +0100

    Copy both Xcode theme definitions

--- run/after/001-Xcode-Theme.sh
+++ run/after/001-Xcode-Theme.sh
@@ -6,5 +6,6 @@ echo "Copying Xcode theme…"
 
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 cp ~/.dotfiles/Xcode/*.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+cp ~/.dotfiles/Xcode/*.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
 echo "Successfully installed Xcode themes."
